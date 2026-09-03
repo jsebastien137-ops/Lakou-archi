271db429e094c6ae5c1ad1803cdc8e79b184cfdd
jsebastien137-ops <jsebastien137@gmail.com>
2026-09-03 02:14:38 -0500

    Implement global error handler for JS errors
    Added global error handler to display JavaScript errors.

--- supabase-client.js
+++ supabase-client.js
@@ -1,3 +1,7 @@
+window.onerror = function(msg, url, line) {
+  alert('Erreur JS : ' + msg + ' (ligne ' + line + ')');
+};
+
 var SUPABASE_URL = 'https://qptnjgdfobznwmsguvyf.supabase.co';
 var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwdG5qZ2Rmb2J6bndtc2d1dnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MjA3MjIsImV4cCI6MjA5MzQ5NjcyMn0.QLfIITvc-AdWVLZHHghocNYyYyYvPxZZMAXhdl_4Bdo';
 var sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
